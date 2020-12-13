a7f984dbc82a7b5a3aafe88d1aac3ddb753e06c2
Felipe Valladao <felipe.valladao@gmail.com>
2020-12-13 23:43:53 +0000

    add styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,34 @@
 import React from "react";
 import home1 from "../img/home1.png";
 
+import {About, Description, Hide, Image} from '../styles';
+
 const AboutSection = () => {
     return (
-        <div className="description">
-            <div className="title">
-                <div className="hide">
-                    <h2>We work to make</h2>
+        <About>
+            <Description>
+                <div className="title">
+                    <Hide>
+                        <h2>We work to make</h2>
+                    </Hide>
+                    <Hide>
+                        <h2>your <span>dreams</span> come</h2>
+                    </Hide>
+                    <Hide>
+                        <h2>true.</h2>
+                    </Hide>
                 </div>
-                <div className="hide">
-                    <h2>your <span>dreams</span> come</h2>
-                </div>
-                <div className="hide">
-                    <h2>true.</h2>
-                </div>
-            </div>
-            <p>
-                Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.
-            </p>
-            <button>Contact us</button>
-            <div className="image">
+                <p>
+                    Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.
+                </p>
+                <button>Contact us</button>
+            </Description>
+            <Image>
                 <img src={home1} alt="Guy with a camera"/>
-            </div>
-        </div>
+            </Image>
+        </About>
     )
 }
+
 
 export default AboutSection;
