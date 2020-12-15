b2d75876053b0772ef96259b48063bbb706fecb2
Felipe Valladao <felipe.valladao@gmail.com>
2020-12-15 22:46:08 +0000

    nav and secondary pages

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { MovieState } from "../movieState";
+
+const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie[0]);
+  }, [movies, url]);
+
+  return (
+    <>
+      {movie && (
+        <Details>
+          <Headline>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie" />
+          </Headline>
+          <Awards>
+            {movie.awards.map((a) => (
+              <Award
+                title={a.title}
+                description={a.description}
+                key={a.title}
+              />
+            ))}
+          </Awards>
+          <ImageDisplay>
+            <img src={movie.secondaryImg} alt="movie" />
+          </ImageDisplay>
+        </Details>
+      )}
+    </>
+  );
+};
+
+const Details = styled.div`
+  color: white;
+`;
+const Headline = styled.div`
+  min-height: 90vh;
+  padding-top: 20vh;
+  position: relative;
+  h2 {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Awards = styled.div`
+  min-height: 80vh;
+  display: flex;
+  margin: 5rem 10rem;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Award = ({ title, description }) => {
+  return (
+    <AwardStyle>
+      <h3>{title}</h3>
+      <div className="line"></div>
+      <p>{description}</p>
+    </AwardStyle>
+  );
+};
+
+const AwardStyle = styled.div`
+  padding: 5rem;
+  h3 {
+    font-size: 2rem;
+  }
+  .line {
+    width: 50%;
+    background: #23d997;
+    margin: 1rem 0rem;
+    height: 0.5rem;
+  }
+`;
+
+const ImageDisplay = styled.div`
+  min-height: 20vh;
+  img {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+`;
+
+export default MovieDetail;
